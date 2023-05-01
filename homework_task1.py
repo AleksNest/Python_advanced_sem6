1c8a8338a769b2aa354aec18de6d5f71260d9126
Alexx <al_nesteov@inbox.ru>
2023-05-01 15:26:50 +0300

    дз к семинару6

---
+++ homework_task1.py
@@ -0,0 +1,12 @@
+'''
+Задача 1
+В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку. .
+'''
+
+
+from module_homework_task1 import calend, calend_tetminal
+
+
+if __name__ == '__main__':
+    print(calend('12.12.2016'))
+    calend_tetminal()
